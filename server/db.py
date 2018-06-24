e4060067b43e5227b1766738fd4aed8b6b802446
shd0908 <jwcu1992@163.com>
2018-06-24 23:23:51 +0800

    log 日志

--- server/db.py
+++ server/db.py
@@ -1,5 +1,5 @@
 from Common.common_OpDB import hh_DB
-
+from Common import logger
 hh = hh_DB('mock_db','../data/config.ini')
 
 def insert_user(u_id,s_name,mobile,province):
@@ -10,10 +10,12 @@ def insert_user(u_id,s_name,mobile,province):
 #
 def select_user(uid):
     sele_sql = "SELECT * FROM mock_user WHERE id = %s" % uid
+    logger.setup_logger("DEBUG")
+    logger.log_debug('正在执行查询sql%s' % sele_sql)
     data = hh.get_data(sele_sql)
 
     return data
 
 if __name__ == '__main__':
     select_user(2)
-    insert_user(25,'孟燕京','18513071628','河南')
+    #insert_user(25,'孟燕京','18513071628','河南')
